93ae178afd58159c189f56b36efb69cf7143ef27
mariemomar <mariemomar29@gmail.com>
2025-02-27 20:51:17 +0200

    fix the problem with the tracking method

--- models/tracking/tracker.py
+++ models/tracking/tracker.py
@@ -1,37 +1,31 @@
 from deep_sort_realtime.deepsort_tracker import DeepSort
 import cv2
 
+class Tracker:
+    def __init__(self):
 
+        self.tracker = DeepSort(max_age=30, n_init=4, embedder="mobilenet")  #mobilenet is a model for extracting features
 
-
-class Tracker :
-    def __init__(self): 
-        # max age -> how many frames to keep searching for person before consider it as lost
-        # n_init -> how many frames the person must be detected to be considered as a valid detection and start tracking it
-        self.tracker = DeepSort(max_age=30 , n_init=4)  
+    def update(self, frame, detections):
         
-    def update(self ,frame , detections):
-        deepSort_detections =[]
-        for detected in detections :
+        deepSort_detections = []
+        for detected in detections:
             bbox = detected['bbox']
-            confidence = detected['confidence']
-            person_type = detected['person_type']
-            deepSort_detections.append((bbox , confidence , person_type))
+            width = bbox[2] - bbox[0]  
+            height = bbox[3] - bbox[1]  
+            deepSort_detections.append(([bbox[0], bbox[1], width, height], detected['confidence'], 0))  
 
-        tracks = self.tracker.update_tracks(deepSort_detections , frame= frame  ) 
+        tracks = self.tracker.update_tracks(deepSort_detections, frame=frame)
 
         for track in tracks:
             if not track.is_confirmed():
                 continue
 
-            trackID = track.track_id
-            bbox = track.to_tlwh()
-            x , y , w  , h = map(int , bbox)
+            track_id = track.track_id
+            bbox = track.to_tlwh()  # Convert to [x, y, w, h] format
+            x, y, w, h = map(int, bbox)
 
-            # cv2.rectangle(frame, (x, y), ( w, h), (0, 255, 0), 2)
-            cv2.putText(frame, f"ID: {trackID}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, f"ID: {track_id}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
         return frame
-
-
-        
