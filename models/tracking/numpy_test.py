ba4dd49802e6390df7488ad50bc29ce160661214
YassenAli <144853147+YassenAli@users.noreply.github.com>
2025-05-11 01:07:33 +0300

    remove test files

--- models/tracking/numpy_test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import torch
-print("NumPy Version:", np.__version__)
-print("PyTorch Version:", torch.__version__)
-print("NumPy Available:", np.array([1,2,3])) 
--- models/tracking/verifyCuda.py
+++
@@ -1,7 +0,0 @@
-import torch
-print(torch.__version__)
-print(torch.cuda.is_available())
-print(torch.version.cuda)
-print(torch.cuda.get_device_name(0))
-print(torch.zeros(1).cuda())
-print(torch.cuda.memory_allocated() / 1e9, 'GB')
